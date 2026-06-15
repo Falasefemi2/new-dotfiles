2df221526df6d2bc4ced8fa2258ea39f6640dfa0
falase femi <femifalase228@gmail.com>
2026-06-15 14:31:14 +0100

    feat: toggle and truobule

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -1,5 +1,33 @@
-vim.pack.add { 'https://github.com/akinsho/toggleterm.nvim' }
-require('toggleterm').setup {
+vim.pack.add({ "https://github.com/akinsho/toggleterm.nvim" })
+
+require("toggleterm").setup({
   open_mapping = [[<C-\>]],
-  direction = 'float',
-}
+})
+
+local Terminal = require("toggleterm.terminal").Terminal
+
+local float_term = Terminal:new({
+  direction = "float",
+})
+
+local vertical_term = Terminal:new({
+  direction = "vertical",
+  size = vim.o.columns * 0.3,
+})
+
+local horizontal_term = Terminal:new({
+  direction = "horizontal",
+  size = 15,
+})
+
+vim.keymap.set("n", "<leader>tf", function()
+  float_term:toggle()
+end, { desc = "Float Terminal" })
+
+vim.keymap.set("n", "<leader>tv", function()
+  vertical_term:toggle()
+end, { desc = "Vertical Terminal" })
+
+vim.keymap.set("n", "<leader>th", function()
+  horizontal_term:toggle()
+end, { desc = "Horizontal Terminal" })
---
+++ lua/custom/plugins/trouble.lua
@@ -0,0 +1,13 @@
+vim.pack.add { 'https://github.com/folke/trouble.nvim' }
+
+require("trouble").setup {
+  -- Add your custom options here if needed (defaults are fine for most people)
+}
+
+-- Keymaps
+vim.keymap.set("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", { desc = "Diagnostics (Trouble)" })
+vim.keymap.set("n", "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", { desc = "Buffer Diagnostics (Trouble)" })
+vim.keymap.set("n", "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>", { desc = "Symbols (Trouble)" })
+vim.keymap.set("n", "<leader>cl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", { desc = "LSP Definitions / references / ... (Trouble)" })
+vim.keymap.set("n", "<leader>xL", "<cmd>Trouble loclist toggle<cr>", { desc = "Location List (Trouble)" })
+vim.keymap.set("n", "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", { desc = "Quickfix List (Trouble)" })
